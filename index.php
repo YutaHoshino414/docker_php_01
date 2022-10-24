91614ca1b79de930ed0d5132adaad5f732e22cae
Yuta Hoshino <y.hoshino25@gmail.com>
2022-10-24 15:23:39 +0900

    Add templates

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <html>
   <head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     <title>PHP Test</title>
   </head>
   <body>
+    <?php include 'templates/header.php'; ?> <!-- header.php の読み込み -->
     <?php echo '<h1>Hello World!!</h1><br>
                 <h2 style="color:red;"> >> phpinfo表示</h2>'; 
-    ?> 
-    <?php phpinfo(); ?>
+    ?>
+    <section class="section is-medium">
+      <h1 class="title">Large section</h1>
+      <h2 class="subtitle">
+        A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading.
+      </h2>
+    </section>
+    <?php include 'templates/footer.php'; ?> <!-- footer.php の読み込み -->
   </body>
 </html>
---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<?php 
+  //echo '<h1>header</h1>'; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+  <section class="hero is-primary">
+  <div class="hero-body">
+    <p class="title">
+      Primary hero
+    </p>
+    <p class="subtitle">
+      Primary subtitle
+    </p>
+  </div>
+  </section>
+</body>
+</html>
